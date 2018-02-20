980d940f4a2418cdca9785f3a74354b631110c89
Tomás Pollak <tomaspollak@gmail.com>
2018-02-20 11:02:51 -0300

    Added list_templates.js example. Fixed proxy return on LinkedElement.get

---
+++ examples/list_templates.js
@@ -0,0 +1,43 @@
+var bootic   = require('../'),
+    helpers  = require('./helpers'),
+    args     = helpers.args();
+
+if (!args.token && !args.clientId) {
+  helpers.usage('list_templates.js');
+}
+
+function listTemplatesIn(theme) {
+  theme.templates.each(function(template) {
+    console.log(template.file_name, template.body.length, template.updated_on)
+  })
+}
+
+process.on('unhandledRejection', function(reason, p) {
+  console.log('Unhandled Rejection at:', p, 'reason:', reason);
+})
+
+bootic
+  .auth(args)
+  .then(function(root) {
+    return root.shops.all();
+  })
+  .then(function(list) {
+    return helpers.selectShopFrom(list);
+  })
+  .then(function(shop) {
+    return shop.themes.get()
+  })
+  .then(function(themes) {
+    if (themes.dev_theme) {
+      console.log('Found dev theme!')
+      return themes.dev_theme.get()
+    }
+
+    return themes.theme.get()
+  })
+  .then(function(theme) {
+    listTemplatesIn(theme)
+  })
+  .catch(function(err) {
+    console.log('err!', err)
+  })
